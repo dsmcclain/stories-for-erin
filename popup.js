5cd54fb0390e61070d6a3cb5b7b66b4a24a3d776
Dylan McClain <dylan.mcclain@gmail.com>
2019-05-01 12:38:04 -0700

    add url to loacl storage

--- popup.js
+++ popup.js
@@ -4,6 +4,10 @@ let savebutton = document.getElementById('savebutton');
 
 savebutton.onclick = function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    alert(tabs[0].url);
+    let savedURL = tabs[0].url;
+    let title = tabs[0].title;
+    chrome.storage.sync.set({title: savedURL}, function() {
+      alert('added to storage: ' + title + ': ' + savedURL);
+    });
   });
 };
